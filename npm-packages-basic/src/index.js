059fa134498de43cb5d6e1a8a806fb354d341dd4
Daniel Ferreira <dcfer2017@gmail.com>
2025-07-01 10:13:10 -0300

    feat: exemplos de gerenciamento de pacotes com node

---
+++ npm-packages-basic/src/index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+console.log(logSymbols.success, "Service started");
+
+console.log(chalk.blue('Hello world!'));
+
+console.log(chalk.blue.bgGray('Hello world!'));
