458c5d96417c36c4d17e2c905d2aa723fbd9d8d4
Daniel Ferreira <dcfer2017@gmail.com>
2025-07-18 09:58:56 -0300

    feat: cria service para listagem de episódios

--- projeto-app-flow/src/controllers/podscats-controller.ts
+++ projeto-app-flow/src/controllers/podscats-controller.ts
@@ -1,8 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http'
+import { serviceListEpisodes } from '../services/list-episodes-service'
 
 export const getListEpisodes = async(req: IncomingMessage, res: ServerResponse) => {
+  const content = await serviceListEpisodes()
+
   res.writeHead(200, { 'content-type': "application/json" })
-  res.end(JSON.stringify({
-    name: "Daniel",
-  }))
+  res.end(JSON.stringify(content))
 }
---
+++ projeto-app-flow/src/services/list-episodes-service.ts
@@ -0,0 +1,7 @@
+
+
+export const serviceListEpisodes = async() => {
+  const data = [{name: "Exemplo"}]
+
+  return data
+}
