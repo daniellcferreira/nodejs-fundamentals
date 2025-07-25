93f2e42b87afcbb13f604a124a28af324a089ec9
Daniel Ferreira <dcfer2017@gmail.com>
2025-07-25 10:41:38 -0300

    feat: adiciona rota GET /players/:id com controller e service de busca por ID

--- projeto-champions/src/services/players-service.ts
+++ projeto-champions/src/services/players-service.ts
@@ -1,9 +1,22 @@
-import { findAllPlayers } from "../repositories/players-repository"
+import { findAllPlayers, findPlayerById } from "../repositories/players-repository"
 import { noContent, ok } from "../utils/http-helper"
 
 
 export const getPlayerService = async () => {
   const data = await findAllPlayers()
+  let response = null
+
+  if (data) {
+    response = ok(data)
+  } else {
+    response = noContent()
+  }
+
+  return response
+}
+
+export const getPlayerByIdService = async (id: number) => {
+  const data = await findPlayerById(id)
   let response = null
 
   if (data) {
